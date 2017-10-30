d163fcbc47621922458e73c343fa68dd6512e622
Brian Hechinger <wonko@4amlunch.net>
2017-10-30 13:14:57 -0400

    First attempt at cleaning it all up to be a public module.

--- Instance.tf
+++ Instance.tf
@@ -18,7 +18,7 @@ data "template_file" "ChefServer_setup_script" {
     admin_fn          = "${var.admin_firstname}"
     admin_ln          = "${var.admin_lastname}"
     admin_email       = "${var.admin_email}"
-    org               = "${var.chef_organization}"
+    org               = "${lower(var.chef_organization)}"
     admin_password    = "${var.admin_password}"
     access_key_id     = "${var.aws_access_key_id}"
     secret_access_key = "${var.aws_secret_access_key}"
--- Outputs.tf
+++ Outputs.tf
@@ -1,3 +1,11 @@
 output "id" {
   value = "${aws_instance.ChefServer.id}"
 }
+
+output "private_ip" {
+  value = "${aws_instance.ChefServer.private_ip}"
+}
+
+output "fqdn" {
+  value = "${var.fqdn}"
+}
