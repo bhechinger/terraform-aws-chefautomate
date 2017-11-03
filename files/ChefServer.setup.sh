f03fb27d2b98d68057bc7d89861b4387dfb7a863
Brian Hechinger <wonko@4amlunch.net>
2017-11-03 13:32:58 -0400

    the yum update actually ends up upgrading a lot of the chef stuff.
    If we don't want to upgrade, we probably shouldn't do that either.

--- files/ChefServer.setup.sh
+++ files/ChefServer.setup.sh
@@ -16,7 +16,9 @@ sudo curl --silent -o /root/getssl https://raw.githubusercontent.com/srvrco/gets
 sudo chmod 755 /root/getssl
 
 sudo yum install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
-sudo yum update -y
+if [ "${upgrade_chef}" == "true" ]; then
+    sudo yum update -y
+fi
 sudo yum install -y python-pip
 sudo pip install boto3 dnspython
 
