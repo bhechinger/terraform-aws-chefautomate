2b7f126395bcc532e6e1a200f071b98118f1efe7
Brian Hechinger <wonko@4amlunch.net>
2017-11-03 13:19:55 -0400

    What the hell am I doing?

--- files/s3_upload.py
+++ files/s3_upload.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import boto3, sys
+from os.path import basename
 
 bucket_name = sys.argv[1]
 validator_key = sys.argv[2]
@@ -10,5 +11,5 @@ region = sys.argv[4]
 s3 = boto3.client('s3')
 s3.create_bucket(Bucket=bucket_name)
 
-s3.upload_file(validator_key, bucket_name, "{r}-{k}".format(r=region, k=validator_key))
-s3.upload_file(admin_key, bucket_name, "{r}-{k}".format(r=region, k=admin_key))
+s3.upload_file(validator_key, bucket_name, "{r}-{k}".format(r=region, k=basename(validator_key)))
+s3.upload_file(admin_key, bucket_name, "{r}-{k}".format(r=region, k=basename(admin_key)))
